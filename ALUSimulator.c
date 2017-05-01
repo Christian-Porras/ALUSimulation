59e44755bf444a8cac3bb1ef62540a59ba0d7210
Chris Porras <cjincolorado@comcast.net>
2017-05-01 17:32:05 -0500

    Version 1.0 of ALUSimulation complete

--- ALUSimulator.c
+++ ALUSimulator.c
@@ -29,6 +29,11 @@ extern void ALUSimulator( RegisterFile theRegisterFile,
 				uint32_t ImmediateValue,
 				uint32_t* Status ) {
 
+
+  uint32_t Rdvalue = 0;
+  uint32_t RdValue_S;
+  uint32_t RdValue_T;
+  RegisterFile_Read(theRegisterFile, Rs, &RdValue_S, Rt, &RdValue_T);
 	printf( ">>ALU: Opcode: %02X; Rs: %02X; Rt: %02X; Rd: %02X;\n",
 				OpCode,
 				Rs,
@@ -40,5 +45,85 @@ extern void ALUSimulator( RegisterFile theRegisterFile,
 				FunctionCode,
 				ImmediateValue );
 
+
+  //If statement to determine whether R type or I type instruction
+  if(OpCode == 00){
+    switch(FunctionCode){
+      case 00 : //SLL
+        Rdvalue = (RdValue_T << ShiftAmt);
+        break;
+
+      case 02 : //SRL
+        Rdvalue = (int)((unsigned int)RdValue_T >> ShiftAmt) ;
+        break;
+
+      case 03 : //SRA
+        Rdvalue = (RdValue_S >> ShiftAmt);
+        break;
+
+      case 04 : //SLLV
+        Rdvalue = (int)((unsigned int)RdValue_S << RdValue_T);
+        break;
+
+      case 06 : //SLRV
+        Rdvalue = (int)((unsigned int)RdValue_S >> RdValue_T);
+        break;
+
+      case 32 : //ADD
+        Rdvalue = RdValue_S + RdValue_T;
+        break;
+
+      case 33 : //ADDU
+        Rdvalue = (int)(unsigned int)RdValue_S + (int)(unsigned int)RdValue_T;
+        break;
+
+      case 34 : //SUB
+        Rdvalue = RdValue_S - RdValue_T;
+        break;
+
+      case 35 : //SUBU
+        Rdvalue = (int)(unsigned int)RdValue_S - (int)(unsigned int)RdValue_T;
+        break;
+
+      case 36 : //AND
+        Rdvalue = RdValue_S & RdValue_T;
+        break;
+
+      case 37 : //OR
+        Rdvalue = RdValue_S | RdValue_T;
+        break;
+
+      case 38 : //XOR
+        Rdvalue = RdValue_S ^ RdValue_T;
+        break;
+
+      case 42 : //SLT
+        Rdvalue = RdValue_S < RdValue_T ? 1 : 0;
+        break;
+
+      case 43 : //SLTU
+        Rdvalue = (int)(unsigned int)RdValue_S < (int)(unsigned int)RdValue_T ? 1 : 0;
+        break;
+    }//end switch
+		//printf("%02X\n", Rdvalue);
+		RegisterFile_Write(theRegisterFile, 1, Rd, Rdvalue);
+
+  }
+  else if(OpCode == 8){
+		Rdvalue = RdValue_S + ImmediateValue;
+		RegisterFile_Write(theRegisterFile, 1, Rt, Rdvalue);
+  }
+	else if(OpCode == 9){
+		Rdvalue = (int)(unsigned int)RdValue_S + ImmediateValue;
+		RegisterFile_Write(theRegisterFile, 1, Rt, Rdvalue);
+	}
+	else if(OpCode == 10){
+		Rdvalue = RdValue_S < ImmediateValue ? 1 : 0;
+		RegisterFile_Write(theRegisterFile, 1, Rt, Rdvalue);
+	}
+	else if(OpCode == 11){
+		Rdvalue = (int)(unsigned int)RdValue_S < ImmediateValue ? 1 : 0;
+		RegisterFile_Write(theRegisterFile, 1, Rt, Rdvalue);
+	}
+
 }
-				
